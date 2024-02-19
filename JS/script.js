75c72d2a2b1128ad49efe820336593febfdb74c8
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-02-20 01:47:52 +0300

    первичный макет главной страницы

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+var cluster = document.getElementsByClassName('cluster')[0];
+
+for (a = 0; a < exercise.length; a++)
+    cluster.innerHTML += `
+    <div class="box">
+        <div>
+            <div class="TD">
+                <a class="title" href="#">${exercise[a].TITLE}</a>
+                <div class="date">${exercise[a].DATE}</div>
+            </div>
+            <svg class="inline-svg-icon icon">
+                <use xlink:href="#${a}"></use>
+            </svg>
+        </div>
+        <p align="center">${exercise[a].NOTE}...</p>
+    </div>`
