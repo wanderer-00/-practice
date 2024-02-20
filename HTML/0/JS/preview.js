93f298aee56e163f16e89c6bb66251a4d961cef3
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-02-20 06:25:55 +0300

    исправление мелких недочётов

--- HTML/0/JS/preview.js
+++ HTML/0/JS/preview.js
@@ -1,11 +1,5 @@
-var array = [
-    'Задание',
-    'Работа',
-    'Сцена'
-];
-
 function preview(b) {
     var preview = document.querySelector('.preview');
 
-    preview.innerHTML = `<iframe style="width:100%;height:100%;border:none" src="FILE/web/${array[b]}/index.html"></iframe>`;
+    preview.innerHTML = `<iframe style="width:100%;height:100%;border:none" src="FILE/web/${files[b].NAME}/index.html"></iframe>`;
 }
---
+++ HTML/5/JS/file_sys.js
@@ -0,0 +1,20 @@
+var files = [
+    {
+        "NAME": "Задание",
+        "ICON": "docx"
+    }
+];
+
+var file_sys = document.getElementsByClassName('file_sys')[0];
+
+for (a = 0; a < files.length; a++) {
+    file_sys.innerHTML += `
+        <div class="box" onclick="preview(${a})">
+            <img src="../../IMG/file icon/${files[a].ICON}.svg">
+            <div class="name">${files[a].NAME}</div>
+            <a href="FILE/${files[a].NAME}.${files[a].ICON}" download title="Скачать файл">
+                <img class="download" src="../../IMG/download.svg" alt="download icon">
+            </a>
+        </div>
+    `
+}
