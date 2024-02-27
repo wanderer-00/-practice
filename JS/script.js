97b2bd12d2c42f91eca6896169658398e2b72886
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-02-27 11:56:44 +0300

    Исправление мелких недочётов

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ var cluster = document.getElementsByClassName('cluster')[0];
 
 for (a = 0; a < exercise.length; a++)
     cluster.innerHTML += `
-    <a class="box" href="#">
+    <a class="box" href="HTML/${a}/index.html">
         <div>
             <h2>${exercise[a].TITLE}</h2>
             <svg class="inline-svg-icon icon">
