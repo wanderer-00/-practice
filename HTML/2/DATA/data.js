f453122f16179cc3b5cbe45f0a1ae569cb1e1738
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-03-26 09:18:44 +0300

    -

--- HTML/2/DATA/data.js
+++ HTML/2/DATA/data.js
@@ -2,5 +2,13 @@ var files = [
     {
         "NAME": "Задание",
         "ICON": "docx"
-    }
+    },
+    {
+        "NAME": "Работа",
+        "ICON": "docx"
+    },
+    {
+        "NAME": "password=123",
+        "ICON": "exe"
+    },
 ]
